2a14208c20cdd2a74de30678fc086e9dd7065886
Pabasara Navod <144713553+Pabs-codes@users.noreply.github.com>
2024-03-27 12:35:28 +0530

    removed unused images

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -1,15 +1,4 @@
-// import React from "react";
-// import Heading from "../components/common/Heading";
-// import UC from "../components/common/UC";
 
-// export default function Booking() {
-//   return (
-//     <>
-//       <Heading heading="Booking" title="Home" subtitle="Booking" />
-//       <UC/>
-//     </>
-//   );
-// }
 
 import React, { useEffect, useState } from "react";
 import Heading from "../components/common/Heading";
